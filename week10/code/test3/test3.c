8a77aa63b2b7f39f0bd3fa70df3473ce1c4aae85
wn <2225671356@qq.com>
2019-11-11 18:37:22 -0800

    week10class2

---
+++ week10/code/test3/test3.c
@@ -0,0 +1,26 @@
+#include "my.h"
+
+int g = 10;
+int main(){
+	int s = 20;
+	static int k = 30;
+	pid_t pid;
+	pid = vfork();
+	if (pid<0){
+		perror("failed vfork!\n");
+		exit(-1);
+	}else if(pid == 0){
+		sleep(2);
+		printf("child pid = %d : &g=%16p,&k=%16p,&s=%16p\n",getpid(),&g,&k,&s);
+		g = 100;
+		k = 300;
+		s = 200;
+		printf("child pid = %d : &g=%d,&k=%d,&s=%d\n",getpid(),g,k,s);
+		sleep(2);
+		_exit(0);
+	}else{
+		printf("\n parent pid=%d : &g=%16p,&k=%16p,&s=%16p\n",getpid(),&g,&k,&s);
+		printf("\n parent pid=%d : &g=%d,&k=%d,&s=%d\n",getpid(),g,k,s);
+		return 0;
+	}
+}
